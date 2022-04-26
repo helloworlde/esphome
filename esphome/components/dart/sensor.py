f244ccc01c7c36b283f509a7f61fce557cc51d1f
helloworlde <hellowoodes@gmail.com>
2022-04-26 17:07:53 +0800

    Add sensor back for dart

---
+++ esphome/components/dart/sensor.py
@@ -0,0 +1,50 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor, uart
+from esphome.const import (
+    CONF_ID,
+    CONF_FORMALDEHYDE,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_PARTS_PER_MILLION,
+    UNIT_MICROGRAMS_PER_CUBIC_METER,
+    UNIT_CELSIUS,
+    UNIT_PERCENT,
+    ICON_MOLECULE_CO2,
+    ICON_FLASK,
+    ICON_CHEMICAL_WEAPON,
+    ICON_GRAIN,
+	STATE_CLASS_MEASUREMENT,
+	DEVICE_CLASS_EMPTY,
+)
+
+DEPENDENCIES = ["uart"]
+
+dart_ns = cg.esphome_ns.namespace("dart")
+DARTComponent = dart_ns.class_("DARTComponent", cg.PollingComponent, uart.UARTDevice)
+
+CONFIG_SCHEMA = cv.All(
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(DARTComponent),
+            cv.Optional(CONF_FORMALDEHYDE): sensor.sensor_schema(
+                unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
+                icon=ICON_CHEMICAL_WEAPON,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_EMPTY,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+		}
+    )
+    .extend(cv.polling_component_schema("20s"))
+    .extend(uart.UART_DEVICE_SCHEMA)
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
+
+    if CONF_FORMALDEHYDE in config:
+        sens = await sensor.new_sensor(config[CONF_FORMALDEHYDE])
+        cg.add(var.set_formaldehyde_sensor(sens))
